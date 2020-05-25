4e38f90c8361d8f3caea95882f3ddcf2aaddaa40
YiPay <toono.umi@gmail.com>
2020-05-25 06:18:15 -0400

    pathctl

--- mcmc.py
+++ mcmc.py
@@ -1,4 +1,7 @@
-from FileReader import freader as fr 
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from FileReader import freader as fr
 import time
 from chronometer import Chronometer
 from numba import vectorize
